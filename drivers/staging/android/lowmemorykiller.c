aeabf7559ee2d36ec86ee4fe00c2b244878c10a5
meevals <slavavkosta249@gmail.com>
2026-02-25 00:26:41 +0200

    Refactor memory calculation in lowmemorykiller
    Updated memory management to use atomic_long_read for managed pages.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -448,9 +448,9 @@ static int get_minfree_scalefactor(gfp_t gfp_mask)
 	unsigned long nr_usable = 0;
 
 	for_each_zone_zonelist(zone, z, zonelist, gfp_zone(gfp_mask))
-		nr_usable += zone->managed_pages;
-
-	return max_t(int, 1, mult_frac(100, nr_usable, totalram_pages));
+		nr_usable += atomic_long_read(&zone->managed_pages);
+	
+	return max_t(int, 1, mult_frac(100, nr_usable, totalram_pages()))
 }
 
 static void mark_lmk_victim(struct task_struct *tsk)
